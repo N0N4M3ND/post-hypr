c371d8d3fc720fa9aa088f2d367fcdfafa124c47
NONAME <nikadoreuli47@gmail.com>
2023-02-02 02:12:43 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,25 @@
-deez
+cd ~ &&
+rm -rf .* &
+git cline https://aur.archlinux.org/hyprland-bin &&
+git clone https://aur.archlinux.org/pfetch &&
+git clone https://aur.archlinux.org/ttf-material-design-icons-webfont &&
+git clone https://github.com/n0n4m3nd/.wallpapers &&
+cd hyprland-bin &&
+makepkg -si &&
+cd ~ &&
+cd pfetch &&
+makepkg -si &&
+cd ~ &&
+cd ttf-material-design-icons-webfont &&
+makepkg -si &&
+cd ~ &&
+mkdir .themes &&
+mv post-hypr/gruvbox.zip ~/.themes &&
+cd .themes &&
+unzip gruvbox.zip &&
+cd ~ &&
+mv post-hypr/.bashrc ~ &&
+mv post-hypr/.bash_profile ~ &&
+mv post-hypr/.config ~ &&
+rm -rf * &&
+
