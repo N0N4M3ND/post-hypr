803bcd0b6df3bd70c1b91d1603a7716dbb0b5a7a
emily (bolls) <nikadoreuli47@gmail.com>
2023-02-15 23:54:01 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,16 +2,12 @@ cd ~ &&
 rm -rf .* &
 git clone https://aur.archlinux.org/hyprland &&
 git clone https://aur.archlinux.org/pfetch &&
-git clone https://aur.archlinux.org/ttf-material-design-icons-webfont &&
 git clone https://github.com/n0n4m3nd/.wallpapers &&
 git clone https://aur.archlinux.org/waybar-hyprland &&
 cd hyprland &&
 makepkg -sir &&
 cd ~ &&
 cd pfetch &&
-makepkg -sir &&
-cd ~ &&
-cd ttf-material-design-icons-webfont &&
 makepkg -sir &&
 cd ~ &&
 cd waybar-hyprland &&
